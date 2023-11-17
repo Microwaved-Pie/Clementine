ef918a3f5a41632cb58e7ebb971c3b755bf08d9f
Microwaved-Pie <144716146+Microwaved-Pie@users.noreply.github.com>
2023-11-17 19:01:29 +0000

    added some stugg

--- Clementine.py
+++ Clementine.py
@@ -1,5 +1,31 @@
 from time import sleep
 from colorama import colorama_text
 
-print("CLEMENTINE & AJ")
-print("A continuation from Season 4")
+# D = Decision
+# ID = Important Decision
+
+while True:
+    print("CLEMENTINE & AJ")
+    print("Episode 1: Where The Flowers Grow")
+
+    D = ("0")
+    while True:
+        D1 = input("""
+0: Info
+1: Start
+                
+
+{: """)
+        print("_________________________________________________________________________")
+        print(" ")
+        if D1  == ("1"):
+            print("{:Start")
+            print("_________________________________________________________________________")
+            print(" ")
+        if D1 == ("0"):
+            print("{:Info")
+            print("""
+Clementine & AJ is a rewrite of the clementine comics. or more or less a rewrite of the events after season 4.
+This is episode 1/4: Where The Flowers Grow""")
+            print("_________________________________________________________________________")
+            print(" ")
